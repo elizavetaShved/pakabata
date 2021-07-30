42f5d9f5b1dc9fc1a80df7880a1a6b43ec875b8c
elizavetaShved <eli3avetta.shvedchikova@gmail.com>
2021-07-30 10:21:25 +0300

    save video-block

--- src/js/component-video.js
+++ src/js/component-video.js
@@ -1,3 +1,15 @@
 export default function componentVideo() {
+  const hostElem = document.querySelector('#video-host');
+  const openVideoBtn = hostElem.querySelector('.video__btn');
+  const closeVideoBtn = hostElem.querySelector('.video__close-btn');
+  const videoContainerElem = hostElem.querySelector('.video__modal-container');
 
+  openVideoBtn.onclick = () => {
+    videoContainerElem.classList.add('video__modal-container--show');
+  }
+
+  closeVideoBtn.onclick = () => {
+    videoContainerElem.classList.remove('video__modal-container--show');
+
+  }
 }
