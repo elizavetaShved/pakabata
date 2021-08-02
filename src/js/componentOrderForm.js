012c99a37d3f0ecd36434d6103871a232a916d0c
elizavetaShved <eli3avetta.shvedchikova@gmail.com>
2021-08-02 16:12:46 +0300

    input number

--- src/js/componentOrderForm.js
+++ src/js/componentOrderForm.js
@@ -2,11 +2,15 @@ import commonDatepicker from './commonDatepicker';
 import { MAX_LENGTH_CHILDREN, MIN_LENGTH_CHILDREN } from './consts';
 
 export default function componentOrderForm() {
+  const INPUT_WIDTH = 42;
+
   const hostElem = document.querySelector('#order-form-host');
   const datepickerElem = hostElem.querySelector('.gl-datepicker');
   const childrenMinusBtn = hostElem.querySelector('#children-btn-minus');
   const childrenPlusBtn = hostElem.querySelector('#children-btn-plus');
   const childrenInput = hostElem.querySelector('#children-input');
+
+  childrenInput.style.width = `${ INPUT_WIDTH }px`
 
   childrenMinusBtn.onclick = () => {
     if (childrenInput.value >= MIN_LENGTH_CHILDREN) {
@@ -37,7 +41,7 @@ export default function componentOrderForm() {
 
   const setWidthInputChildren = () => {
     const additionalWidth = (String(childrenInput.value).length - 1) * 8;
-    childrenInput.style.width = `${ 42 + additionalWidth }px`;
+    childrenInput.style.width = `${ INPUT_WIDTH + additionalWidth }px`;
   }
 
   commonDatepicker(datepickerElem);
