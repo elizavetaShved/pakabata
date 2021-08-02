51095888378ffda8b8d4f5d16f0f7146bd545889
elizavetaShved <eli3avetta.shvedchikova@gmail.com>
2021-08-02 16:09:48 +0300

    input number

--- src/js/componentOrderForm.js
+++ src/js/componentOrderForm.js
@@ -1,8 +1,44 @@
 import commonDatepicker from './commonDatepicker';
+import { MAX_LENGTH_CHILDREN, MIN_LENGTH_CHILDREN } from './consts';
 
 export default function componentOrderForm() {
   const hostElem = document.querySelector('#order-form-host');
-  const datepicker = hostElem.querySelector('.gl-datepicker');
+  const datepickerElem = hostElem.querySelector('.gl-datepicker');
+  const childrenMinusBtn = hostElem.querySelector('#children-btn-minus');
+  const childrenPlusBtn = hostElem.querySelector('#children-btn-plus');
+  const childrenInput = hostElem.querySelector('#children-input');
 
-  commonDatepicker(datepicker);
+  childrenMinusBtn.onclick = () => {
+    if (childrenInput.value >= MIN_LENGTH_CHILDREN) {
+      childrenInput.value--;
+      setWidthInputChildren();
+    }
+  }
+
+  childrenPlusBtn.onclick = () => {
+    if (childrenInput.value <= MAX_LENGTH_CHILDREN) {
+      childrenInput.value++;
+      setWidthInputChildren();
+    }
+  }
+
+  childrenInput.oninput = () => {
+    switch (true) {
+      case childrenInput.value < MIN_LENGTH_CHILDREN:
+        childrenInput.value = MIN_LENGTH_CHILDREN;
+        break;
+
+      case childrenInput.value > MAX_LENGTH_CHILDREN:
+        childrenInput.value = MAX_LENGTH_CHILDREN;
+        break;
+    }
+    setWidthInputChildren();
+  }
+
+  const setWidthInputChildren = () => {
+    const additionalWidth = (String(childrenInput.value).length - 1) * 8;
+    childrenInput.style.width = `${ 42 + additionalWidth }px`;
+  }
+
+  commonDatepicker(datepickerElem);
 }
