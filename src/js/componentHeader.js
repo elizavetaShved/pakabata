11aa91780fd0df0650b9e52fb7f4c55bd61a22d9
elizavetaShved <eli3avetta.shvedchikova@gmail.com>
2021-08-05 10:27:09 +0300

    close btn sidebar

--- src/js/componentHeader.js
+++ src/js/componentHeader.js
@@ -1,19 +1,26 @@
-import { lockScroll, unlockScroll } from './scrollBlocker';
+import { lockScroll } from './scrollBlocker';
+import { checkExistParent } from './checkExistParent';
 
 export default function componentHeader() {
   const hostElem = document.querySelector('#header-host');
   const burgerBtn = hostElem.querySelector('.header__burger-wrapper');
-  const sidebarContainer = hostElem.querySelector('.header__sidebar-container');
-  const sidebarWrapper = hostElem.querySelector('.header__sidebar-wrapper');
+  const sidebarContainerElem = hostElem.querySelector('.header__sidebar-container');
+  const sidebarWrapperElem = hostElem.querySelector('.header__sidebar-wrapper');
+  const closeBtn = hostElem.querySelector('.header__sidebar-close-btn-wrapper');
 
   burgerBtn.onclick = () => {
-    sidebarContainer.classList.add('mod-open');
-    lockScroll(sidebarWrapper);
+    sidebarContainerElem.classList.add('mod-open');
+    lockScroll(sidebarWrapperElem);
   }
 
-  sidebarContainer.onclick = (e) => {
-    if (e.target === sidebarContainer) {
-      sidebarContainer.classList.remove('mod-open');
+  sidebarContainerElem.onclick = (e) => {
+    if (e.target === sidebarContainerElem || checkExistParent(e.target, closeBtn)) {
+      sidebarContainerElem.classList.remove('mod-open');
     }
+  }
+
+  closeBtn.onclick = () => {
+    sidebarContainerElem.classList.add('mod-open');
+    lockScroll(sidebarWrapperElem);
   }
 }
