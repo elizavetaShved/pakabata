410f6ce443b95f7894482d805d9b1e21dfa06703
Elizaveta <eli3avetta@mail.ru>
2021-08-05 21:15:46 +0300

    #

--- src/js/componentVideo.js
+++ src/js/componentVideo.js
@@ -1,15 +1,33 @@
+import { checkExistParent } from './checkExistParent';
+import { lockScroll, unlockScroll } from './scrollBlocker';
+
 export default function componentVideo() {
   const hostElem = document.querySelector('#video-host');
   const openVideoBtn = hostElem.querySelector('.video__btn');
   const closeVideoBtn = hostElem.querySelector('.video__close-btn');
   const videoContainerElem = hostElem.querySelector('.video__modal-container');
+  const videoModalWrapperElem = hostElem.querySelector('.gl-video-modal-wrapper');
 
   openVideoBtn.onclick = () => {
-    videoContainerElem.classList.add('mod-show');
+    onLockScroll();
   }
 
   closeVideoBtn.onclick = () => {
-    videoContainerElem.classList.remove('mod-show');
+    onUnlockScroll();
+  }
 
+  videoContainerElem.onclick = (e) => {
+    if (!(checkExistParent(e.target, videoModalWrapperElem))) {
+      onUnlockScroll();
+    }
+  }
+
+  const onLockScroll = () => {
+    videoContainerElem.classList.add('mod-show');
+    lockScroll(videoContainerElem);
+  }
+  const onUnlockScroll = () => {
+    videoContainerElem.classList.remove('mod-show');
+    unlockScroll(videoContainerElem);
   }
 }
