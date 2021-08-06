fdf6ed0b5ce414cb852ff292705871743b793405
elizavetaShved <eli3avetta.shvedchikova@gmail.com>
2021-08-06 13:02:26 +0300

    слайдер

--- src/js/componentBanner.js
+++ src/js/componentBanner.js
@@ -1,16 +1,23 @@
-import { Navigation, Swiper, Mousewheel, Pagination } from 'swiper';
+import { EffectFade, Mousewheel, Navigation, Pagination, Swiper } from 'swiper';
 
 Swiper.use([Navigation]);
 Swiper.use([Mousewheel]);
 Swiper.use([Pagination]);
+Swiper.use([EffectFade]);
 
 export default function componentBanner() {
   const hostElem = document.querySelector('#banner-host');
+  const slidesListElems = hostElem.querySelectorAll('.banner__slide');
+
+  let currentSlide = 0;
 
   const swiper = new Swiper(hostElem.querySelector('.banner__container'), {
     pagination: {
       el: '.swiper-pagination'
     },
+    effect: 'fade',
+    speed: 0.000000001,
+    grabCursor: true,
     slidesPerView: 1.12,
     spaceBetween: 8,
     centeredSlides: true,
@@ -29,6 +36,22 @@ export default function componentBanner() {
           prevEl: '.swiper-button-prev',
         }
       }
+    }
+  });
+
+  swiper.on('slideChange', () => {
+    if (currentSlide < swiper.activeIndex) {
+      slidesListElems[swiper.activeIndex].classList.add('mod-next-slide');
+      setTimeout(() => {
+        slidesListElems[swiper.activeIndex].classList.remove('mod-next-slide');
+      }, 250)
+      currentSlide++;
+    } else {
+      slidesListElems[swiper.activeIndex].classList.add('mod-prev-slide');
+      setTimeout(() => {
+        slidesListElems[swiper.activeIndex].classList.remove('mod-prev-slide');
+      }, 250)
+      currentSlide--;
     }
   });
 }
