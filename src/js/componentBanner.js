6401fe0a26ca8207e232b13995a92f3e487de87e
Elizaveta <eli3avetta@mail.ru>
2021-08-07 17:22:04 +0300

    save

--- src/js/componentBanner.js
+++ src/js/componentBanner.js
@@ -1,7 +1,6 @@
 import { EffectFade, Mousewheel, Navigation, Pagination, Swiper } from 'swiper';
 
 Swiper.use([Navigation]);
-Swiper.use([Mousewheel]);
 Swiper.use([Pagination]);
 Swiper.use([EffectFade]);
 
@@ -9,27 +8,36 @@ export default function componentBanner() {
   const hostElem = document.querySelector('#banner-host');
   const slidesListElems = hostElem.querySelectorAll('.banner__slide');
 
-  let currentSlide = 0;
+  // const commonSwiperParams = {
+  //   pagination: {
+  //     el: '.swiper-pagination'
+  //   },
+  //   centeredSlides: true,
+  //   virtual: true,
+  //   lazy: true,
+  //   keyboard: true,
+  //   speed: 400,
+  // }
 
   const swiper = new Swiper(hostElem.querySelector('.banner__container'), {
     pagination: {
       el: '.swiper-pagination'
     },
-    grabCursor: true,
     slidesPerView: 1.12,
     spaceBetween: 8,
     centeredSlides: true,
     virtual: true,
     lazy: true,
     keyboard: true,
+    effect: 'fade',
+    speed: 400,
     breakpoints: {
       640: {
+        effect: 'slide',
         slidesPerView: 1.08,
         spaceBetween: 16,
       },
       1024: {
-        effect: 'fade',
-        speed: 0.000000001,
         slidesPerView: 1,
         navigation: {
           nextEl: '.swiper-button-next',
@@ -39,19 +47,34 @@ export default function componentBanner() {
     }
   });
 
-  swiper.on('slideChange', () => {
-    if (currentSlide < swiper.activeIndex) {
-      slidesListElems[swiper.activeIndex].classList.add('mod-next-slide');
-      setTimeout(() => {
-        slidesListElems[swiper.activeIndex].classList.remove('mod-next-slide');
-      }, 250)
-      currentSlide++;
-    } else {
-      slidesListElems[swiper.activeIndex].classList.add('mod-prev-slide');
-      setTimeout(() => {
-        slidesListElems[swiper.activeIndex].classList.remove('mod-prev-slide');
-      }, 250)
-      currentSlide--;
+  const onResize = () => {
+    if (window.innerWidth <= 1024) {
+
     }
+  }
+
+  swiper.on('slideNextTransitionStart', () => {
+    slidesListElems[swiper.activeIndex].classList.add('mod-next-slide');
   });
+
+  swiper.on('slideNextTransitionEnd', () => {
+    clear();
+  });
+
+  swiper.on('slidePrevTransitionStart', () => {
+    slidesListElems[swiper.activeIndex].classList.add('mod-prev-slide');
+    slidesListElems[swiper.activeIndex + 1].classList.add('mod-prev-slide-up');
+  });
+
+  swiper.on('slidePrevTransitionEnd', () => {
+    clear();
+  });
+
+  const clear = () =>
+    slidesListElems.forEach(elem =>
+      elem.classList.remove('mod-next-slide', 'mod-prev-slide', 'mod-prev-slide-up'));
+
+  window.addEventListener('resize', () => {
+    onResize();
+  })
 }
