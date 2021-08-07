2801b3d0e3d03d5be20bf212d4fe479244f7cfaf
Elizaveta <eli3avetta@mail.ru>
2021-08-07 13:59:13 +0300

    fix header

--- src/js/componentHeader.js
+++ src/js/componentHeader.js
@@ -7,26 +7,38 @@ export default function componentHeader() {
   const sidebarContainerElem = hostElem.querySelector('.header__sidebar-container');
   const sidebarWrapperElem = hostElem.querySelector('.header__sidebar-wrapper');
   const closeBtn = hostElem.querySelector('.header__sidebar-close-btn-wrapper');
+  const geolocationLinkSidebar = sidebarContainerElem.querySelector('.header__geolocation');
+  const menuItemsArr = hostElem.querySelectorAll('.header__menu-item');
 
   burgerBtn.onclick = () => {
-    onLockScroll();
+    openSidebar();
   }
 
   sidebarContainerElem.onclick = (e) => {
     if (e.target === sidebarContainerElem || checkExistParent(e.target, closeBtn)) {
-      onUnlockScroll();
+      closeSidebar();
     }
   }
 
-  closeBtn.onclick = () => {
-    onLockScroll();
+  geolocationLinkSidebar.onclick = () => {
+    closeSidebar();
   }
 
-  const onLockScroll = () => {
+  closeBtn.onclick = () => {
+    openSidebar();
+  }
+
+  menuItemsArr.forEach(menuItem => {
+    menuItem.onclick = () => {
+      closeSidebar();
+    }
+  })
+
+  const openSidebar = () => {
     sidebarContainerElem.classList.add('mod-open');
     lockScroll(sidebarWrapperElem);
   }
-  const onUnlockScroll = () => {
+  const closeSidebar = () => {
     sidebarContainerElem.classList.remove('mod-open');
     unlockScroll(sidebarWrapperElem);
   }
