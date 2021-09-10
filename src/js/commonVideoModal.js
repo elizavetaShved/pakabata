73b947673aca9695019f3567ffc4c7caa4c31139
Elizaveta <eli3avetta@mail.ru>
2021-09-10 10:18:06 +0300

    управление видео mp4 (controls)

--- src/js/commonVideoModal.js
+++ src/js/commonVideoModal.js
@@ -7,26 +7,31 @@ export default function commonVideoModal(hostElem) {
   const videoContainerElem = hostElem.querySelector('.gl-modal-container');
   const videoModalWrapperElem = hostElem.querySelector('.gl-modal-wrapper');
 
-  const onLockScroll = () => {
+  const videoMp4Elems = document.querySelectorAll('.gl-video-mp4');
+
+  const onOpen = () => {
     videoContainerElem.classList.add('mod-show');
     lockScroll(videoContainerElem);
   }
-  const onUnlockScroll = () => {
+  const onClose = () => {
     videoContainerElem.classList.remove('mod-show');
     unlockScroll(videoContainerElem);
+    videoMp4Elems.forEach(video => {
+      video.load();
+    })
   }
 
   openVideoBtn.onclick = () => {
-    onLockScroll();
+    onOpen();
   }
 
   closeVideoBtn.onclick = () => {
-    onUnlockScroll();
+    onClose();
   }
 
   videoContainerElem.onclick = (e) => {
     if (!(checkExistParent(e.target, videoModalWrapperElem))) {
-      onUnlockScroll();
+      onClose();
     }
   }
 }
