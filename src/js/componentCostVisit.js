f9ae13d6b95e671e2dbb22cc8c8bf49e0ce68fa6
Elizaveta <eli3avetta@mail.ru>
2021-09-12 17:29:02 +0300

    подарочные сертефикаты + autopay на видео убрано

--- src/js/componentCostVisit.js
+++ src/js/componentCostVisit.js
@@ -1,64 +1,71 @@
 export default function componentCostVisit() {
-    const TRANSLATE_X_STEP = 18;
+  const TRANSLATE_X_STEP_CAPTION = 20;
+  const TRANSLATE_X_STEP_CAPTION_MOBILE = 14;
+  const TRANSLATE_X_STEP_VALUE = 18;
 
-    const hostElem = document.querySelector('#cost-visit-host');
-    const toggleBtns = hostElem.querySelectorAll('.cost-visit__toggle-item');
-    const toggleBackground = hostElem.querySelector('.cost-visit__toggle-background');
-    const valuesSliderWrapper = hostElem.querySelectorAll('.cost-visit__price-grid-values-slider');
-    const valuesSmallArr = hostElem.querySelectorAll('.cost-visit__price-grid-value-small');
-    const valuesFullArr = hostElem.querySelectorAll('.cost-visit__price-grid-value-full');
+  const hostElem = document.querySelector('#cost-visit-host');
+  const toggleBtns = hostElem.querySelectorAll('.cost-visit__toggle-item');
+  const toggleBackground = hostElem.querySelector('.cost-visit__toggle-background');
+  const captionsSliderWrapper = hostElem.querySelectorAll('.cost-visit__price-grid-caption-slider');
+  const valuesSliderWrapper = hostElem.querySelectorAll('.cost-visit__price-grid-values-slider');
 
-    const priceItemElems = hostElem.querySelectorAll('.cost-visit__price-grid-item');
-    const priceCaptionsElems = hostElem.querySelectorAll('.cost-visit__price-grid-caption');
+  const valuesSmallArr = hostElem.querySelectorAll('.cost-visit__price-grid-value-small');
+  const valuesFullArr = hostElem.querySelectorAll('.cost-visit__price-grid-value-full');
 
-    let activeSlide = 0;
+  const priceItemElems = hostElem.querySelectorAll('.cost-visit__price-grid-item');
 
-    toggleBtns.forEach((btnItem, index) => {
-        btnItem.onclick = () => {
-            if (index !== activeSlide) {
-                setActiveSlide(index);
-            }
-        }
-    })
+  let activeSlide = 0;
 
-    const setActiveSlide = (index) => {
-        activeSlide = index;
-        if (index === 0) {
-            toggleBtns[0].classList.add('mod-active');
-            toggleBtns[1].classList.remove('mod-active');
-            toggleBackground.classList.remove('mod-right');
-            valuesSliderWrapper.forEach(valuesSliderWrapperItem => {
-                valuesSliderWrapperItem.style.transform = 'translateX(0rem)';
-            })
-            valuesSmallArr.forEach(valueSmallItem => {
-                valueSmallItem.classList.add('mod-active');
-            })
-            valuesFullArr.forEach(valuesFullItem => {
-                valuesFullItem.classList.remove('mod-active');
-            })
-
-            priceItemElems[1].classList.remove('mod-hide');
-
-            priceCaptionsElems.forEach(elem => {
-                elem.classList.remove('mod-hide');
-            })
-        } else {
-            toggleBtns[0].classList.remove('mod-active');
-            toggleBtns[1].classList.add('mod-active');
-            toggleBackground.classList.add('mod-right');
-            valuesSliderWrapper.forEach(valuesSliderWrapperItem => {
-                valuesSliderWrapperItem.style.transform = `translateX(-${ TRANSLATE_X_STEP }rem`;
-            })
-            valuesSmallArr.forEach(valueSmallItem => {
-                valueSmallItem.classList.remove('mod-active');
-            })
-            valuesFullArr.forEach(valuesFullItem => {
-                valuesFullItem.classList.add('mod-active');
-            })
-
-            priceItemElems[1].classList.add('mod-hide');
-
-            priceCaptionsElems[1].classList.add('mod-hide');
-        }
+  toggleBtns.forEach((btnItem, index) => {
+    btnItem.onclick = () => {
+      if (index !== activeSlide) {
+        setActiveSlide(index);
+      }
     }
+  })
+
+  const setActiveSlide = (index) => {
+    activeSlide = index;
+    if (index === 0) {
+      toggleBtns[0].classList.add('mod-active');
+      toggleBtns[1].classList.remove('mod-active');
+      toggleBackground.classList.remove('mod-right');
+      captionsSliderWrapper.forEach(captionSliderWrapperItem => {
+        captionSliderWrapperItem.style.transform = 'translateX(0rem)';
+      })
+      valuesSliderWrapper.forEach(valuesSliderWrapperItem => {
+        valuesSliderWrapperItem.style.transform = 'translateX(0rem)';
+      })
+      valuesSmallArr.forEach(valueSmallItem => {
+        valueSmallItem.classList.add('mod-active');
+      })
+      valuesFullArr.forEach(valuesFullItem => {
+        valuesFullItem.classList.remove('mod-active');
+      })
+
+      priceItemElems[1].classList.remove('mod-hide');
+    } else {
+      toggleBtns[0].classList.remove('mod-active');
+      toggleBtns[1].classList.add('mod-active');
+      toggleBackground.classList.add('mod-right');
+      captionsSliderWrapper.forEach(captionSliderWrapperItem => {
+        if (window.innerWidth >= 567) {
+          captionSliderWrapperItem.style.transform = `translateX(-${ TRANSLATE_X_STEP_CAPTION }rem`;
+        } else {
+          captionSliderWrapperItem.style.transform = `translateX(-${ TRANSLATE_X_STEP_CAPTION_MOBILE }rem`;
+        }
+      })
+      valuesSliderWrapper.forEach(valuesSliderWrapperItem => {
+        valuesSliderWrapperItem.style.transform = `translateX(-${ TRANSLATE_X_STEP_VALUE }rem`;
+      })
+      valuesSmallArr.forEach(valueSmallItem => {
+        valueSmallItem.classList.remove('mod-active');
+      })
+      valuesFullArr.forEach(valuesFullItem => {
+        valuesFullItem.classList.add('mod-active');
+      })
+
+      priceItemElems[1].classList.add('mod-hide');
+    }
+  }
 }
