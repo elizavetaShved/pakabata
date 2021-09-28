0a4ae7ae654bea1932984000ae3d7cefac2f171c
Elizaveta <eli3avetta@mail.ru>
2021-09-28 11:01:18 +0300

    fix

--- src/js/scrollByAnchor.js
+++ src/js/scrollByAnchor.js
@@ -1,7 +1,7 @@
 export default function scrollByAnchor() {
   $("a[href^='#']").click(function(){
     const _href = $(this).attr("href");
-    $("html, body").animate({scrollTop: $(_href).offset().top+'px'});
+    $("html, body").animate({scrollTop: $(_href).offset().top+'px'}, 2000);
     return false;
   });
 }
